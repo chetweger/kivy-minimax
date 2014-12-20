38e353c3bb117af0a37397e66f2d1a58d6de07c0
Chester (Chet) Karl Weger <chetweger@gmail.com>
2014-12-20 15:32:11 -0800

    Added constants.

---
+++ ai/constants.h
@@ -0,0 +1,11 @@
+struct constants {
+    float c1 = 3.1;
+    float c2 = 1.05;
+    float c3 = 0.76;
+    float c4 = 0.60;
+    float c5 = 0.28;
+    float c6 = 0.15;
+};
+
+struct constants td_constants;
+
